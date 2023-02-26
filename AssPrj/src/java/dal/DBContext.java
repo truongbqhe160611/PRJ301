be01fe87d9f1b556e6418b8dac1da3f319e355db
truongbqhe160611 <buitruong091102@gmail.com>
2023-02-27 02:52:02 +0700

    27/2

--- AssPrj/src/java/dal/DBContext.java
+++ AssPrj/src/java/dal/DBContext.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
 
 import java.sql.Connection;
@@ -9,26 +5,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
 /**
  *
  * @author FPT University - PRJ301
  */
 public class DBContext {
-
     protected Connection connection;
-
-    public DBContext() {
+    public DBContext()
+    {
         try {
             String user = "sa";
             String pass = "12345678";
-            String url = "jdbc:sqlserver://DESKTOP-FQ5NO5A\\SQLEXPRESS:1433;databaseName=master";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=test";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
     // Kiem tra ket noi toi CSDL
     public static void main(String[] args) {
         System.out.println(new DBContext().connection);
